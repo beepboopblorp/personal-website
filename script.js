1759b79e1e2f05dbf17cf1512e6b932c2bf917c0
eesha <79874478+AlicornEagle11@users.noreply.github.com>
2024-06-27 16:36:19 +0000

    fixed theme, spelling, grammar, and content errors

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-function lightmode() {
-  let lightbutton = document.body;
-  lightbutton.className = "light-mode";
-  lightbutton.classList.toggle("light-mode");
-}
+const btn = document.querySelector(".btn-toggle");
+const theme = document.querySelector("#theme-link");
+const duckimg = document.getElementById("duck");
+
+btn.addEventListener("click", function mode() {
+  if (theme.getAttribute("href") == "light.css") {
+    theme.href = "dark.css";
+    duckimg.src = "https://www.motosha.com/files/preview/2000x1333/26024-yellow-duckling-portrait-black-background.jpg";
+  } else {
+    theme.href = "light.css";
+    duckimg.src = "https://www.warrenphotographic.co.uk/photography/bigs/06514-Yellow-duckling-white-background.jpg";
+  }
+});
